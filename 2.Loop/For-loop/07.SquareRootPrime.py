4eb71f185a2b82f29c3ce831ca3dd5277760d444
Suhana <suhanabagwan27@gmail.com>
2026-07-29 20:39:52 +0530

    Implement square root prime check in new script
    Add a script to check if the square root of a number is prime.

---
+++ 2.Loop/For-loop/07.SquareRootPrime.py
@@ -0,0 +1,21 @@
+import math
+
+num = int(input("Enter number: "))
+root = int(math.sqrt(num))
+
+prime = True
+
+if root < 2:
+    prime = False
+else:
+    for i in range(2, root):
+        if root % i == 0:
+            prime = False
+            break
+
+print("Square root =", root)
+
+if prime:
+    print("Square root is Prime")
+else:
+    print("Square root is Not Prime")
