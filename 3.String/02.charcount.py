cddccfb356417218f833eae755c40180f0f06350
Suhana <suhanabagwan27@gmail.com>
2026-07-30 11:40:31 +0530

    Add character counting program in 02.charcount.py
    This program counts the number of vowels, consonants, digits, spaces, and special characters in a given string.

---
+++ 3.String/02.charcount.py
@@ -0,0 +1,23 @@
+# Program to count vowels, consonants, digits, spaces and special characters
+
+string = input("Enter a string: ")
+
+vowels = consonants = digits = spaces = special = 0
+
+for ch in string:
+    if ch.lower() in "aeiou":
+        vowels += 1
+    elif ch.isalpha():
+        consonants += 1
+    elif ch.isdigit():
+        digits += 1
+    elif ch.isspace():
+        spaces += 1
+    else:
+        special += 1
+
+print("Vowels:", vowels)
+print("Consonants:", consonants)
+print("Digits:", digits)
+print("Spaces:", spaces)
+print("Special Characters:", special)
